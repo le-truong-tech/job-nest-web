8d970931ca6d01003d83614870aca75774b38d1e
le-truong-tech <le.truong.tech@gmail.com>
2026-09-07 23:06:10 +0700

    chore: init JobNest FE

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-red-200">helloworl</h1>
+     <Button>Tìm việc ngay</Button>
+    </>
+  )
+}
+
+export default App
